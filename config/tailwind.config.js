34b76c30c9519a23daca8a965cc85b52b78c5690
Kirstine Peñalber <penalberkirstine@gmail.com>
2021-01-10 10:27:01 +0800

    Documentation: Add BG Image Config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: theme => ({
+        'hero-pattern': "url('img/hero-pattern.svg')",
+        'footer-texture': "url('img/footer-texture.png')"
+      })
       colors: {
         gray: {
           100: '#FBFBFB',
